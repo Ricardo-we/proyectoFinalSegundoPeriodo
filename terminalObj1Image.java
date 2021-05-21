fd952c9aa2a115d35d97dd218618564f2f56e9b0
Ricardo-we <78930182+Ricardo-we@users.noreply.github.com>
2021-05-21 10:01:49 -0600

    Add files via upload

---
+++ terminalObj1Image.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package terminal;
+
+/**
+ *
+ * @author Sonia
+ */
+public class terminalObj1Image {
+    
+   public terminalObj1Image(){
+        System.out.println("                               ( ( (                   \n" +
+"                                ) ) )                 \n" +
+"                               ( ( (          \n" +
+"                             '. ___ .'       \n" +
+"                            '  (> <) '        \n" +
+"                    --------ooO-(_)-Ooo----------\n" +
+"                            BIENVENIDO ");
+    }
+   
+}
